636c2542ecae27dc361f9afc186aaa06f9ff7d0f
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-09-30 17:01:06 +0545

    Add project files.

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleTemplate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var res = 'n';
+            do
+            {
+                //input your logic here.
+
+                Console.Write("Do you want to continue more (y/n)?  ");
+                res = Console.ReadKey().KeyChar;
+                Console.Clear();
+            } while (res == 'y' || res == 'Y');
+        }
+    }
+}
